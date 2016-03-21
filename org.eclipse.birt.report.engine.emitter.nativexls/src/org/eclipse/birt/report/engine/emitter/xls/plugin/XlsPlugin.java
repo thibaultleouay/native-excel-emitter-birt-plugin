0faf7e7d5d18ba642c6872a8b350cf55e63995e6
Thibault Le Ouay <thibaultleouay@gmail.com>
2016-03-21 14:08:36 +0100

    Change to eclipse plugin instead of birt plugin

--- org.eclipse.birt.report.engine.emitter.nativexls/src/org/eclipse/birt/report/engine/emitter/xls/plugin/XlsPlugin.java
+++ org.eclipse.birt.report.engine.emitter.nativexls/src/org/eclipse/birt/report/engine/emitter/xls/plugin/XlsPlugin.java
@@ -1,13 +1,18 @@
 package org.eclipse.birt.report.engine.emitter.xls.plugin;
 
-import org.eclipse.birt.core.plugin.BIRTPlugin;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-public class XlsPlugin extends BIRTPlugin
+public class XlsPlugin extends AbstractUIPlugin
 {
 
-    public void start( BundleContext context ) throws Exception
-    {
-        super.start( context );
-    }
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+	
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
 }
