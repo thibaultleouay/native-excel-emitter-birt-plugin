159152e3f305e21dd3713a7171ee1757cc7698c8
Thibault Le Ouay <thibaultleouay@gmail.com>
2016-04-11 16:58:05 +0200

    Remove dependancies to org.Eclipse.ui.workbench
    Signed-off-by: Thibault Le Ouay <thibaultleouay@gmail.com>

--- org.eclipse.birt.report.engine.emitter.nativexls/src/org/eclipse/birt/report/engine/emitter/xls/plugin/XlsPlugin.java
+++ org.eclipse.birt.report.engine.emitter.nativexls/src/org/eclipse/birt/report/engine/emitter/xls/plugin/XlsPlugin.java
@@ -1,18 +1,19 @@
 package org.eclipse.birt.report.engine.emitter.xls.plugin;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-public class XlsPlugin extends AbstractUIPlugin
+public class XlsPlugin extends Plugin
 {
 
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 	}
-	
+
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
 	}
+
 }
